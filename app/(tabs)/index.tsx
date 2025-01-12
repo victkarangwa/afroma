83abcb7579aa1974102e6c656705e22e2d827c95
Victor Karangwa <victkarangwa@gmail.com>
2025-01-12 13:54:22 +0200

    tab change action

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -73,17 +73,13 @@ const HomeScreen: React.FC = () => {
     }, 3000);
   };
 
-  const navigate = ()=>{
-    router.push({pathname: "/payment"});
-  }
-
   return (
     <ScreenContainer showHeader={true} title="Home">
       <View style={[tw.flex, tw.flexRow]}>
         {tabs.map((tab, index) => (
           <Button
             key={index}
-            onPress={navigate}
+            onPress={() => onTabChange(index)}
             style={[
               tw.mX4,
               activeTab === index && tw.borderB2,

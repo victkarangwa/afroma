f5ba535a3f5d5e9412d448eb0e80ad3230f6bb2c
Victor Karangwa <victkarangwa@gmail.com>
2025-02-19 10:47:06 +0200

    update Web client ID

--- components/SocialLogin/index.tsx
+++ components/SocialLogin/index.tsx
@@ -6,7 +6,7 @@ import { AccessToken, LoginManager } from "react-native-fbsdk-next";
 // export default function App() {
   GoogleSignin.configure({
     webClientId:
-      "720223876750-o6g0kkpikqpdi6nglpg20nntop0f6uuk.apps.googleusercontent.com",
+      "104521571553-70cj474frto1ascll6mfukhilpveqdvp.apps.googleusercontent.com",
   });
  export async function onGoogleButtonPress() {
     try {

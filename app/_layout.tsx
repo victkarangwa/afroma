4121f2a46a6a553abf7e82e48e9a91fd928dacb9
Victor Karangwa <victkarangwa@gmail.com>
2025-02-19 18:59:30 +0200

    force light theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme() === "dark" ? "light" : "light"; // force light theme
   const [loaded] = useFonts({
     SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
   });

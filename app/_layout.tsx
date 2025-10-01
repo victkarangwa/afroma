1409f696d7759c8a0f3f770d3621e95407c99e02
Victor Karangwa <victkarangwa@gmail.com>
2025-10-01 12:55:43 +0200

    fix(layout): add headerBackTitle to improve navigation clarity in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -172,6 +172,7 @@ export default function RootLayout() {
                 options={{
                   headerTitle: "View Post",
                   headerTitleAlign: "center",
+                  headerBackTitle: "Forums",
                 }}
               />
               <Stack.Screen
